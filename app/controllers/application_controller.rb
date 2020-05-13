0f03b7b22cd6778520be6d46642753d09ed37e73
Elijah wines <elijahwines84@yahoo.com>
2020-05-13 18:08:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,48 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  #Index Action 
+  get '/recipes' do 
+    @recipe = Recipe.all
+    erb :index
+  end
+
+  #New Action
+  get '/recipes/new' do 
+    erb :new
+  end
+
+  post '/recipes' do 
+    @recipe = Recipe.create(:name = params[:name], :recipe = params[:recipe], :cook_time = params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  #Show Action
+  get '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  #Edit Action
+  get '/recipes/:id/edit' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #Delete Action
+  delete '/recipes/:id' do 
+    @reecipe = Recipe.find_by_id(params[:id])
+    @recipe.delete 
+    redirect to "/recipes"
+  end
 
 end
